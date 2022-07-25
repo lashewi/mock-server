66f7556d1a7f6da31ff015bcad04c6d28da0794a
lashew <swisum@gmail.com>
2022-07-25 16:16:54 +0530

    feat: run shell script

---
+++ run-me.sh
@@ -0,0 +1,4 @@
+echo "----------------------------------------------------"
+echo "Deploying mock server"
+docker-compose -f docker-compose.yml up --build -d
+echo "----------------------------------------------------"
